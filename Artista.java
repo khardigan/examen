133a221a082bb7e5f7136bd7baa6fd9a1dc7c811
https://github.com/JesusFPCadiz/ExamenGIT.git <https://github.com/JesusFPCadiz/ExamenGIT.git>
2024-03-14 16:39:46 +0100

    cambios artista

--- Artista.java
+++ Artista.java
@@ -1,22 +1,27 @@
 package evf.javadoc.original;
 
 public class Artista {
+    // constante de la clase
     String nombreArtista;
-
     public Artista(String nombreArtista) {
         this.nombreArtista = nombreArtista;
     }
-
+   //toString
     @Override
     public String toString() {
         return "Artista{" +
                 "nombreArtista='" + nombreArtista + '\'' +
                 '}';
     }
-
+   //metodo que retorna nombreArtista
     public String getNombreArtista() {
         return nombreArtista;
 	//Esto es un comentario
+    }
+	//comentario de jose 
+   //metodo que devuelve el nombre
+    public String cadenaArtista(){
+	return "el nombre es :"+nombreArtista;
     }
     
 }
